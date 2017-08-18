d4737e7d53f38ec264f039e13018d57419f98a47
stefani spayde <spayde.stefani@gmail.com>
2017-08-18 00:42:44 -0500

    minor adjustment in syntax, removed coma after last property in object

---
+++ public/js/app.js
@@ -0,0 +1,82 @@
+const app = angular.module('MyApp', []);
+
+app.controller('MyController', ['$http', function($http){
+    let newBeer;
+    const controller = this;
+    // function to get beer data
+    this.getBeer = function(){
+      // ajax call to url to get beer data
+      $http({
+          method: 'GET',
+          url: '/beer',
+      }).then(
+          function(response){
+              controller.beers = response.data;
+          },
+          function(error){
+
+          }
+      )
+    }
+    // function to create new beer
+    this.createBeer = function(){
+      $http({
+          method: 'POST',
+          url: '/beer',
+          data: {
+              varietal: this.varietal,
+              brand: this.brand,
+              name: this.name,
+              abv: this.abv
+          }
+      }).then(
+          function(response){
+            console.log(response);
+              controller.getBeer();
+          },
+          function(err){
+
+          }
+      );
+    }
+    // function to edit beer
+    this.editBeer = function(beer){
+      $http({
+          method: 'PUT',
+          url: '/beer/' + newBeer._id,
+          data: {
+              varietal: this.updatedVarietal,
+              brand: this.updatedBrand,
+              name: this.updatedName,
+              abv: this.updatedAbv
+          }
+      }).then(
+          function(response){
+            controller.getBeer();
+        },
+          function(err){
+
+        }
+      )
+    }
+    // function to delete beer
+    this.deleteBeer = function(beer){
+        $http({
+            method: 'DELETE',
+            url: '/beer/' + beer._id,
+        }).then(
+            function(response){
+                controller.getBeer();
+            },
+            function(err){
+
+            }
+        )
+    }
+    this.toggle = true;
+
+this.toggleView =  () => {
+  this.toggle = !this.toggle;
+};
+    this.getBeer();
+}])
